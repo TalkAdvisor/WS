2f2930a0bd37dec30c7b44473ed0af23c1ef826f
Evan Chen <evanhope.chen@gmail.com>
2016-08-10 15:39:44 +0800

    1. register: if facebook id, the pwd will be empty 2. getUser update, add several result 3. add getReview and getQuote

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,11 +21,7 @@ class UserController extends Controller
         $password = Input::get('password');
         if (Auth::attempt(array('email' => $email, 'password' => $password))){
         	$user = User::where('email','=',$email)->first();
-        	if ($user->hasRole('user')) { 
-			    return $this->response->array(['message'=>'success','user' => $user->toArray()]);
-			}else{
-				return $this->response->array(['message'=>'Wrong role']);
-			}
+			return $this->response->array(['message'=>'success','user' => $user->toArray()]);
         }
         else {
             return $this->response->array(['message'=>'Wrong Credentials']);
@@ -35,17 +31,36 @@ class UserController extends Controller
     public function getUser($id)
     {
     	$user = User::find($id);
-        $reviews = Review::where('user_id', '=', $user->id)->orderBy('id', 'desc')->get();
-        $comments_count =  Review::where('user_id', '=', $user->id)->whereNotNull('comment')->count();
-        $quotes_count =  Review::where('user_id', '=', $user->id)->whereNotNull('quote')->count();
-        
         if ($user) { 
+            $reviews = Review::where('user_id', '=', $user->id)->orderBy('id', 'desc')->get();
+            $comments_count =  Review::where('user_id', '=', $user->id)->whereNotNull('comment')->count();
+            $quotes_count =  Review::where('user_id', '=', $user->id)->whereNotNull('quote')->count();
+            $ratings_count = 0;
+            $total = array();
+            $average = array();
+            //total and ratings count
+            foreach($reviews as $review){
+                $ratings = $review->review_options()->get();
+                if(count($ratings)>0){
+                    $ratings_count++;
+                    for($count=0;$count < count($ratings); $count++){
+                       $total[$count] = ((count($total) < 5) ? 0:$total[$count]);
+                       $total[$count] += $ratings[$count]->pivot->score_id; 
+                    }
+                }
+            }
+            //calculate average
+            for($count=0;$count < count($total); $count++){
+                $average[] = round($total[$count]/$ratings_count,2); 
+            }
+
 			return $this->response->array([
                 'user' => $user->toArray(), 
                 'reviews' => $reviews->toArray(), 
                 'comments_count' => $comments_count,
-                //'ratings_count' => $review_data,
-                'quotes_count' => $quotes_count]);
+                'ratings_count' => $ratings_count,
+                'quotes_count' => $quotes_count,
+                'average' => $average]);
 		}else{
 			return $this->response->array(['message'=>'The user is not exist']);
 		}
@@ -83,10 +98,6 @@ class UserController extends Controller
               File::delete('uploads/users/'.$old_img);
             }
             $user->save();
-            if(!$user->hasRole('user')){
-                $userRole = Role::where('slug','=','user')->first();
-                $user->attachRole($userRole->id);
-            }
             return $this->response->array(['user' => $user->toArray()]);
         }
         catch(\Exception $e){
@@ -99,11 +110,14 @@ class UserController extends Controller
     {
     	try{
             $s3 = Storage::disk('s3');
+            $fb_id = $request->input('facebook_id');
             $user = new User;
             $user->name = $request->input('name');
             $user->email = $request->input('email');
-            $user->password = bcrypt($request->input('password'));
-            $user->facebook_id = $request->input('facebook_id');
+            if($fb_id == null){
+                $user->password = bcrypt($request->input('password'));
+            }
+            $user->facebook_id = $fb_id;
             $user->phone_number = $request->input('phone_number');
             $file = $request->file('image');
             if($file != null){
@@ -135,48 +149,46 @@ class UserController extends Controller
         //$password = Input::get('password');
         $user = User::where('email','=',$email)->first();
 
-        if ($user){
-            if($user->hasRole('user')){
-                $user_facebook_id = $user->facebook_id;
-                if($user_facebook_id == $fb_id && $user_facebook_id != null){
-                    return $this->response->array(['status'=>'1','message'=>'The account is existd in talkadvisor.','user' => $user->toArray()]);
-                }else{
-                    return $this->response->array(['status'=>'2','message'=>'The account is existd in talkadvisor, but no facebook id.']);
-                } 
+        if ($user){          
+            $user_facebook_id = $user->facebook_id;
+            if($user_facebook_id == $fb_id && $user_facebook_id != null){
+                return $this->response->array(['status'=>'1','message'=>'The account is existd in talkadvisor.','user' => $user->toArray()]);
             }else{
-                return $this->response->array(['status'=>'3','message'=>'The account is existd in talkadvisor, but no user permission.']);
+                return $this->response->array(['status'=>'2','message'=>'The account is existd in talkadvisor, but no facebook id.']);
             }       
         }
         else {
-            return $this->response->array(['status'=>'4','message'=>'No account in talkadvisor.']);
+            return $this->response->array(['status'=>'3','message'=>'No account in talkadvisor.']);
         }
     }
 
-    /*public function getStats($id){
-        $stats = [];
-        $reviewsCollection = Review::where('user_id',$id);
-        $reviews=$reviewsCollection->get();
-        
-        $stats['number_ratings']=$reviews->count();
-        $stats['number_comments']=$reviewsCollection->where('comment','!=',"")->count();
-        $stats['number_quotes']=$reviewsCollection->where('quote','!=',"")->count();
-        
-        for($i=0;$i<=5;$i++){                                       //initialisation of the averages
-            $stats["average_$i"]=0;
+    public function getReview($id)
+    {
+        $user = User::findOrFail($id);
+        $reviews = Review::where('user_id', '=', $user->id)->orderBy('id', 'desc')->get();
+        $review_with_rating = array();
+        $review_data = array();
+        foreach($reviews as $review){
+          $review_with_relation = $review->toArray();
+          $review_with_relation['speaker'] = $review->speaker;
+          unset($review_with_relation['speaker_id']);
+          unset($review_with_relation['user_id']);
+
+          $review_with_rating['review'] = $review_with_relation;
+          $review_with_rating['review_rating'] = $review->review_options()->get();
+          $review_data[] = $review_with_rating;
         }
-        if($stats['number_ratings']!=0){
-            foreach($reviews as $review){                               //we sum over all the ratings                           
-                $ratings=RatingsController::getRatings($review->id);
-                foreach($ratings as $rating){
-                    $i = $rating->ratingoption_id;
-                    $stats["average_$i"]+=$rating->score;
-                }
-            }
-            for($i=0;$i<=5;$i++){                                       //we divide by the number of ratings
-                $stats["average_$i"]=$stats["average_$i"]/$stats['number_ratings'];
-            } 
+        return $this->response->array(['user' => $user->toArray(), 'reviews' => $review_data]);
+    }
+
+    public function getQuote($id)
+    {
+        $user = User::findOrFail($id);
+        $reviews = Review::where('user_id', '=', $user->id)->whereNotNull('quote')->get();
+        foreach($reviews as $review){
+          $quote[] = $review->quote;
+          $speaker[] = $review->speaker;
         }
-        
-        return $stats;
-    }*/
+        return $this->response->array(['speaker' => $speaker, 'quotes' => $quote]);
+    }
 }
